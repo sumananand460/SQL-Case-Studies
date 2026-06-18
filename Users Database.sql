4bb15102719f943c9908a0496af34b53a5b5a69f
Suman Anand <sumananand460@gmail.com>
2026-06-18 12:04:19 +0530

    Modified new file

--- Users Database.sql
+++ Users Database.sql
@@ -1,13 +1,46 @@
--- Creates the users database container
-CREATE DATABASE users_db;
-USE users_db;
+-- Filtering and Sorting Data
+-- Lets create a new database named employeee
+create database employee;
+use employee;
 
--- In MySQL, use AUTO_INCREMENT (with an underscore) and VARCHAR instead of TEXT
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(150) UNIQUE NOT NULL,
-    age INT,
-    city VARCHAR(100)
+create table employees (
+	emp_id int auto_increment,
+    emp_name varchar(50) not null,
+    department varchar(50) not null,
+    salary decimal (10,2) not null,
+    primary key (emp_id)
 );
-show databases
+
+insert into employees (emp_name, department, salary) values
+("Suman Annd", "CSE", 20000),
+("Kisan Kumar", "HR", 30000),
+("Priya", "Accounts", 15000),
+("Rogers", "Finance", 25000),
+("Shanti", "CSE", 20000);
+
+select *
+from employees;
+
+select *
+from employees
+where department = "CSE";
+
+select *
+from employees
+where salary >= 20000;
+
+select *
+from employees
+where department = "CSE"
+and salary > 10000;
+
+SELECT *
+FROM employees
+WHERE department = 'CSE'
+OR department = 'Finance';
+
+SELECT *
+FROM employees
+WHERE NOT department = 'CSE';
+
+
